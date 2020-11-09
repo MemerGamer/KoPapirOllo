0d2ae807b5af8f3afe80b534482b13540b6d5904
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2020-11-09 17:30:19 +0200

    Add files via upload

---
+++ KoPapirOllo/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace KoPapirOllo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(45, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kő";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(45, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Papír";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(45, 268);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 57);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Olló";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KoPapirOllo.Properties.Resources.qq;
+            this.pictureBox1.Location = new System.Drawing.Point(249, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::KoPapirOllo.Properties.Resources.qq;
+            this.pictureBox2.Location = new System.Drawing.Point(812, 122);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(203, 203);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(316, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 44);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Te";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(871, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 40);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "CPU";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(582, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(38, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 39);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Körök száma:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(537, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Eredmény";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(258, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "3";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1009, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(333, 360);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(901, 360);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(930, 20);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Hátralevő idő:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 413);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ KoPapirOllo/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KoPapirOllo
+{
+    public partial class Form1 : Form
+    {
+        public int rounds = 3; //3 kor/game
+        public int time = 6; //5sec/kor
+        string[]AIchoice={"ko","papir","ollo","ko","papir","ollo"};
+        public int randNumber;
+        string command;
+        Random rnd = new Random();
+        string playerChoice;
+        int playerWins = 0;
+        int AIwins = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            playerChoice = "none";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void checkGame()
+        {
+            if(playerChoice=="ko" && command =="papir")
+                {
+                
+                AIwins++;
+                rounds--;
+                MessageBox.Show("A gep nyert haha");
+                nextRound();
+
+                }
+            else if(playerChoice=="papir" && command=="ko")
+                {
+                
+                playerWins++;
+                rounds--;
+                MessageBox.Show("Te nyertel yey");
+                nextRound();
+                }
+            else if(playerChoice=="papir" && command=="ollo")
+            {
+            
+            AIwins++;
+            rounds--;
+            MessageBox.Show("A gep nyert hah");
+            nextRound();
+            }
+            else if(playerChoice=="ollo" && command=="papir")
+            {
+            
+            playerWins++;
+            rounds--;
+            MessageBox.Show("Te nyerte yeh");
+            nextRound();
+            }
+            else if(playerChoice=="ollo" && command=="ko")
+            {
+            
+            AIwins++;
+            rounds--;
+            MessageBox.Show("A bot nyert haha");
+            nextRound();
+            }
+            else if(playerChoice=="ko" && command=="ollo")
+            {
+            
+            playerWins++;
+            rounds--;
+            MessageBox.Show("Te nyertel yey");
+            nextRound();
+            }
+            else if (playerChoice=="none")
+            {
+                MessageBox.Show(label1.Text + "Donts mar bazmeg");
+                nextRound();
+                //rounds--;
+                
+            }
+            else
+            {
+                MessageBox.Show("Dontetlen");
+                nextRound();
+                //rounds--;
+                
+            }
+            label9.Text = Convert.ToString(AIwins);
+            label8.Text = Convert.ToString(playerWins);
+        }
+        private void decisionEngine()
+        {
+            if(playerWins>AIwins)
+            {
+                label5.Text=label1.Text + " nyerted a jatekot";
+            }
+            else if (playerWins < AIwins)
+            {
+                label5.Text = "A gep nyerte a jatekot";
+            }
+            else 
+            {
+                label5.Text = "Dontetlen";
+            }
+           
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label6.Text = Convert.ToString(rounds);
+            time--;
+            label7.Text = Convert.ToString(time);
+            if (time == 0)
+            {
+                //timer1.Enabled = false;
+                timer1.Stop();
+                time = 6;
+                randNumber = rnd.Next(0, 5);
+                command = AIchoice[randNumber];
+                switch (command)
+                {
+                    case "ko":
+                        pictureBox2.Image = Properties.Resources.rock;
+                        break;
+                    case "papir":
+                        pictureBox2.Image = Properties.Resources.paper;
+                        break;
+                    case "ollo":
+                        pictureBox2.Image = Properties.Resources.scissors;
+                        break;
+                    default:
+                        break;
+                }
+                if (rounds >= 1)
+                {
+                    checkGame();
+                    //label5.Text = Convert.ToString(rounds);
+
+                }
+                else
+                {
+                    decisionEngine();
+                }
+            }
+            
+
+
+            
+        }
+        private void nextRound()
+        {
+            playerChoice = "none";
+            pictureBox1.Image = Properties.Resources.qq;
+            timer1.Start();
+            pictureBox2.Image=Properties.Resources.qq;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            playerChoice = "ko";
+            pictureBox1.Image=Properties.Resources.rock;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            playerChoice = "papir";
+            pictureBox1.Image=Properties.Resources.paper;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            playerChoice = "ollo";
+            pictureBox1.Image=Properties.Resources.scissors;
+        }
+    }
+}
